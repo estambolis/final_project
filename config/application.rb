8b3ecf7a68736ddb645d74991f2c4e64bf64d328
Elias Stambolis <estambol@chicagobooth.edu>
2015-06-10 12:21:56 -0500

    added constant for states

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,61 @@ module Eated
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    def us_states
+        [
+      ['AK', 'AK'],
+    ['AL', 'AL'],
+    ['AR', 'AR'],
+    ['AZ', 'AZ'],
+    ['CA', 'CA'],
+    ['CO', 'CO'],
+    ['CT', 'CT'],
+    ['DC', 'DC'],
+    ['DE', 'DE'],
+    ['FL', 'FL'],
+    ['GA', 'GA'],
+    ['HI', 'HI'],
+    ['IA', 'IA'],
+    ['ID', 'ID'],
+    ['IL', 'IL'],
+    ['IN', 'IN'],
+    ['KS', 'KS'],
+    ['KY', 'KY'],
+    ['LA', 'LA'],
+    ['MA', 'MA'],
+    ['MD', 'MD'],
+    ['ME', 'ME'],
+    ['MI', 'MI'],
+    ['MN', 'MN'],
+    ['MO', 'MO'],
+    ['MS', 'MS'],
+    ['MT', 'MT'],
+    ['NC', 'NC'],
+    ['ND', 'ND'],
+    ['NE', 'NE'],
+    ['NH', 'NH'],
+    ['NJ', 'NJ'],
+    ['NM', 'NM'],
+    ['NV', 'NV'],
+    ['NY', 'NY'],
+    ['OH', 'OH'],
+    ['OK', 'OK'],
+    ['OR', 'OR'],
+    ['PA', 'PA'],
+    ['RI', 'RI'],
+    ['SC', 'SC'],
+    ['SD', 'SD'],
+    ['TN', 'TN'],
+    ['TX', 'TX'],
+    ['UT', 'UT'],
+    ['VA', 'VA'],
+    ['VT', 'VT'],
+    ['WA', 'WA'],
+    ['WI', 'WI'],
+    ['WV', 'WV'],
+    ['WY', 'WY']
+      ]
+  end
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
